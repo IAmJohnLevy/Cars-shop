e2ac728be13df8eab04c348d1c31bde70f954dcb
John_Levy <lev.balura@gmail.com>
2025-05-15 02:40:04 +0300

    feat: add styles and service, fix angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SystemValueService } from "./services/system-value.service";
+import { PageName } from "./enums/page-name";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'cars-shopV1';
+  title = "cars-shop";
+  currentPage: PageName = PageName.MainPage;
+  constructor(private systemValueService: SystemValueService) {}
+
+  ngOnInit() {
+    this.systemValueService.currentPage.subscribe((currentPage) => {
+      this.currentPage = currentPage;
+    });
+  }
 }
